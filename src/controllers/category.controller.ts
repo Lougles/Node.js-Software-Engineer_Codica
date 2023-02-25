364f482d5c1565cf59c9dbc45007ce5fb2d30a91
vovacelidze <louglescom@gmail.com>
2023-02-25 11:56:57 +0200

    added category CRUD funcs to controller

--- src/controllers/category.controller.ts
+++ src/controllers/category.controller.ts
@@ -1,7 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CategoryService } from '../services/category.service';
-import { ResponseModel, successResponse } from '../models/response.model';
+import {
+  emptySuccessResponse,
+  ResponseModel,
+  successResponse,
+} from '../models/response.model';
 import { Category } from '../entities/category.entity';
+import {
+  CategoryCreateModel,
+  CategoryDeleteModel,
+  CategoryUpdateModel,
+} from '../models/category.model';
 
 @Controller('category')
 export class CategoryController {
@@ -11,5 +20,34 @@ export class CategoryController {
   async getAll(): Promise<ResponseModel<Category[]>> {
     const categories = await this.service.getAll();
     return successResponse(categories);
+  }
+  @Get('getOne')
+  async getOne(@Body() body: { id: string }): Promise<ResponseModel<Category>> {
+    const category = await this.service.getOne(body.id);
+    return successResponse(category);
+  }
+
+  @Post('create')
+  async create(
+    @Body() body: CategoryCreateModel,
+  ): Promise<ResponseModel<Category>> {
+    const newCategory = await this.service.create(body);
+    return successResponse(newCategory);
+  }
+
+  @Post('update')
+  async update(
+    @Body() body: CategoryUpdateModel,
+  ): Promise<ResponseModel<Category>> {
+    const updateBank = await this.service.update(body);
+    return successResponse(updateBank);
+  }
+
+  @Post('delete')
+  async delete(
+    @Body() body: CategoryDeleteModel,
+  ): Promise<ResponseModel<void>> {
+    await this.service.delete(body);
+    return emptySuccessResponse();
   }
 }
