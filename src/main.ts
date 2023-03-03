2183372a392882d5c4272c9ce74abe13e7a938c3
vovacelidze <louglescom@gmail.com>
2023-03-03 14:06:18 +0200

    change route to swager doc

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
     .addTag('codica')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('', app, document);
   await app.listen(3000);
   app.use(bodyParser.json({ limit: '2mb' }));
   app.useGlobalPipes(new ValidationPipe({ whitelist: false }));
