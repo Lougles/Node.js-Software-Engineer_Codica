dd054de2dd8a74655a62b38c03a097d154731037
vovacelidze <louglescom@gmail.com>
2023-03-06 18:10:02 +0200

    added json swagger file

--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,14 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as bodyParser from 'body-parser';
 import { ValidationPipe } from '@nestjs/common';
 import { GlobalExceptionFilter } from './utils/exceptions';
-import * as express from 'express';
-import * as path from 'path';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
     .setTitle('Nodejs-Software-Engineer_Codica')
     .setDescription('The API description')
     .setVersion('1.0')
-    .addTag('codica')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  app.use('/api', express.static(path.join(__dirname, 'public/swagger-ui')));
   SwaggerModule.setup('api', app, document);
   await app.listen(3000);
   app.use(bodyParser.json({ limit: '2mb' }));
